5d7e2de879cc12df4f13829001e77123d10edf87
Lucas Dias <lucas.dias@magically.com.br>
2020-05-02 11:43:55 -0300

    Config IIS

--- Program.cs
+++ Program.cs
@@ -19,6 +19,6 @@ namespace TreinoAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                   .UseStartup<Startup>();
     }
 }
